d5e100e84e1024d9db6932fba500fc0f8726873d
siripurapusailakshmi8 <132888069+siripurapusailakshmi8@users.noreply.github.com>
2026-03-18 14:08:00 +0530

    Create pages/base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    """Base Page with common helpers. This file will be expanded in subsequent edits (PLACEHOLDER).
+    """
+    # PLACEHOLDER
