73a293126adc78e81dae53816acafb7e6b1b798e
siripurapusailakshmi8 <132888069+siripurapusailakshmi8@users.noreply.github.com>
2026-03-18 14:10:57 +0530

    Implement OrderHistoryPage methods

--- pages/order_history_page.py
+++ pages/order_history_page.py
@@ -1,6 +1,14 @@
+from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 class OrderHistoryPage(BasePage):
-    """Order history page actions (PLACEHOLDER)
-    """
-    # PLACEHOLDER
+    """Order history page actions."""
+
+    LATEST_ORDER = (By.CSS_SELECTOR, ".order-row")
+    ORDER_ID = (By.CSS_SELECTOR, ".order-id")
+
+    def open_latest_order(self):
+        self.click(*self.LATEST_ORDER)
+
+    def get_latest_order_id(self):
+        return self.get_text(*self.ORDER_ID)
